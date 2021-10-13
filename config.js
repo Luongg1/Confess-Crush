b9e136cdffb2f16edc8d4cd2fc5142a509761091
thluongg <92452553+thluongg@users.noreply.github.com>
2021-10-13 22:40:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "",
-    introTitle: 'Minh Tuan',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introTitle: 'Helloooo ^^',
+    introDesc: `Làm theo lời tui nhe`,
+    btnIntro: 'Bấm vô đây nè ^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Còn cậu?',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3 Đố anh bắt được em',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích mình?',
     btnReply: 'Gửi cho cậu <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Yeu minh la phai mua tra sua cho minh nhaaaaaa',
+    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <33333333',
+    mess: 'Mình biết mà 🥰. Love uuuuuuu',
+    messDesc: 'Yeu minh la phai mua tra sua cho minh nhaaaaaa, mau mau ib Luong lien diiii',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/BeLuongcute/'
 }
